af95d2fc4e90b4c88ed6a5d250f3688bbf6dbd7b
Davo Brunstein <davidbrunstein@gmail.com>
2017-12-25 23:42:45 -0600

    data products prepared

--- coder-swag/coder-swag/Services/DataService.swift
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -18,7 +18,58 @@ class DataService {
         Category(title: "HATS", imageName: "hats.png"),
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
+    
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", price: "$18", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo Hat Black", price: "$20", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Hat White", price: "$18", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo Snapback", price: "$15", imageName: "hat04.png")
+    ]
+    private let hoodies = [
+        Product(title: "Devslopes Logo Hoodie Grey", price: "$30", imageName: "hoodie01.png"),
+        Product(title: "Devslopes Logo Hoodie Red", price: "$32", imageName: "hoodie02.png"),
+        Product(title: "Devslopes Logo Hoodie  White", price: "$33", imageName: "hoodie03.png"),
+        Product(title: "Devslopes Hoodie Black", price: "$34", imageName: "hoodie04.png")
+    ]
+    private let shirts = [
+        Product(title: "Devslopes Logo Shirt Grey", price: "$10", imageName: "shirt01.png"),
+        Product(title: "Devslopes Logo Shirt Red", price: "$12", imageName: "shirt02.png"),
+        Product(title: "Devslopes Logo Shirt  White", price: "$13", imageName: "shirt03.png"),
+        Product(title: "Devslopes Shirt Black", price: "$14", imageName: "shirt04.png"),
+        Product(title: "Devslopes Shirt Pink", price: "$15", imageName: "shirt05.png")
+    ]
+    private let digitalProducts = [Product]()
+
     func getCategories() -> [Category] {
         return categories
     }
+    func getProducts(forCategoryTitle title:String) -> [Product] {
+        switch title {
+        case "SHIRTS":
+            return getShirts()
+        case "HOODIES":
+            return getHoodies()
+        case "HATS":
+            return getHats()
+        case "DIGITAL":
+            return getDititalGoods()
+       default:
+            return getShirts()
+        }
+    }
+    
+    func getHats() -> [Product] {
+        return hats
+    }
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    func getShirts() -> [Product] {
+        return shirts
+    }
+    func getDititalGoods() -> [Product] {
+        return digitalProducts
+    }
+
+    
 }
