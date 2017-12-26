ef0d50666df287b1e7e2c77c38cba300160826fe
Davo Brunstein <davidbrunstein@gmail.com>
2017-12-25 23:22:46 -0600

    Products collection view cell setup

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by David Brunstein on 2017-12-25.
+//  Copyright © 2017 David Brunstein. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
